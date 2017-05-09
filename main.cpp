4025c54b163d32271d5fb510a7000c7670a49e62
presuit <chldudgh8256@naver.com>
2017-05-09 21:09:03 +0900

    ver2.0

--- main.cpp
+++ main.cpp
@@ -7,11 +7,13 @@ using namespace std;
 class Pin
 {
 public:
-	Pin() : type(' '), power(0), defence(0) {};
+	Pin() : Pin(' ', 0, 0) {};
 	Pin(char pin, int setpow, int setdef) : type(pin), power(setpow), defence(setdef) {};
 
+	void move(Pin &target);
 	void battle();
 	void defeated();
+	bool get_is_move();
 
 	friend ostream& operator <<(ostream& os, const Pin& p);
 
@@ -22,6 +24,36 @@ private:
 	int defence;
 };
 
+void Pin::move(Pin &target) 
+{
+	Pin temp;
+
+	temp.type = type;
+	temp.power = power;
+	temp.defence = defence;
+	temp.is_move = true;
+
+	cout << "temp.type : " << temp.type << ", temp.power : " << temp.power << ", temp.defence : " << temp.defence << "temp.ismove : " << temp.is_move << endl;
+
+	type = target.type;
+	power = target.power;
+	defence = target.defence;
+	is_move = target.is_move;
+	
+	cout << "type : " << type << ", power : " << power << ", defence : " << defence << ", ismove : " << is_move << endl;
+
+	target.type = temp.type;
+	target.power = temp.power;
+	target.defence = temp.defence;
+	target.is_move = temp.is_move;
+	
+	cout << "target.type  : " << target.type << "target.power : " << target.power << ", target.defence : " << target.defence << ", target.ismove : " << target.is_move << endl;
+}
+
+bool Pin::get_is_move() 
+{
+	return is_move;
+}
 
 ostream& operator <<(ostream& os, const Pin& p) 
 {
@@ -45,11 +77,103 @@ void print_stage();
 
 int main()
 {
-	do
-	{
+	int menu;
+	int x, y;
+	int whereto;
+	bool isright = false;
+
 		set_stage1();
-		print_stage();
-	} while (Game_Mode  != 4);
+
+		while (Game_Mode != 4) 
+		{
+			print_stage();
+			cout << "1. Move. 2. Battle 3. Submit" << endl;
+			cin >> menu;
+			switch (menu)
+			{
+			case 1:
+				cout << "Input target x, y coordinates" << endl;
+				cout << "X : ";
+				cin >> x;
+				cout << "Y : ";
+				cin >> y;
+
+				if (game_board[x][y].get_is_move() == true) 
+				{
+					cout << "you already moved that Pin!" << endl;
+					continue;
+				}
+				cout << "you choose " << game_board[x][y] << ". where to move ?" << endl;
+				while (isright == false) 
+				{
+					cout << "1. Up  2. Left  3. Right  4. Down" << endl;
+					cin >> whereto;
+					switch (whereto)
+					{
+					case 1:
+						//up
+						if (x - 1 >= 0 && x - 1 < game_board.size())
+						{
+							game_board[x][y].move(game_board[x - 1][y]);
+							isright = true;
+						}
+						else
+						{
+							cout << "Not valid position!" << endl;
+						}
+						break;
+					case 2:
+						//left
+						if (y - 1 >= 0 && y - 1 < game_board[x].size())
+						{
+							game_board[x][y].move(game_board[x][y - 1]);
+							isright = true;
+						}
+						else
+						{
+							cout << "Not valid position!" << endl;
+						}
+						break;
+					case 3:
+						//right
+						if (y + 1 >= 0 && y + 1 < game_board[x].size())
+						{
+							game_board[x][y].move(game_board[x][y + 1]);
+							isright = true;
+						}
+						else
+						{
+							cout << "Not valid position!" << endl;
+						}
+						break;
+					case 4:
+						//down
+						if (x + 1 >= 0 && x + 1 < game_board.size())
+						{
+							game_board[x][y].move(game_board[x + 1][y]);
+							isright = true;
+						}
+						else
+						{
+							cout << "Not valid position!" << endl;
+						}
+						break;
+					default:
+						cout << "Input wrong answer please Input value again" << endl;
+					}
+				}
+				break;
+			case 2:
+				cout << "hi I'm menu 2" << endl;
+				break;
+			case 3:
+				cout << "hi I'm menu 3" << endl;
+				break;
+			default:
+				cout << "input wrong answer please Input again" << endl;
+			}
+		}
+			
 	return 0;
 }
 
